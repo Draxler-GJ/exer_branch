b22a55f1c1f1baf4b7acc14821b0f132ab63e6e1
Draxler-GJ <danjorgom@alu.edu.gva.es>
2025-11-10 23:43:38 +0100

    Commit modificacion

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# exer_branch
+# ESTA ES UNA PREUBA PARA EL REPO exer_branch
+## Practicar de cara al futuro saber desarrollar proyectos
+
+
+| Tabla | Usuario |
+|-------!---------|
+| Prueba| Draxler-GJ|
---
+++ include/process.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Procesado Git-GitHub</title>
+</head>
+<body>
+    <p>
+        <?php
+
+        $prueba = $_POST["prueba"];
+
+        echo '<span color="lime">'.$prueba.'</span>;
+    ?>
+    </p>
+</body>
+</html>
